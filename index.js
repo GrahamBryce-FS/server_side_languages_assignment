d919a99c1fd8a11c995ae54ec938fc63edd77740
Bryce Graham <bgraham1@student.fullsail.edu>
2023-04-09 14:05:48 +0900

    assignment working and functional

--- index.js
+++ index.js
@@ -10,26 +10,21 @@ app.get('/products/all', (req,res)=>{
     const page = req.query.page || 2;
     const sort = req.query.sort || 'price';
     const order = req.query.order || 'desc';
-  
     const responseString = `GET Products: page=${page}, sort=${sort}, order=${order}`;
-  
     res.send(responseString);
 })
 
 // GET /products/8719-small-red HTTP/1.1
 app.get('/products/:productId-:productName', (req,res)=>{
     res.send(
-        'Product Page! Product name: ' + req.params.productName+
-        ' Product id: '+req.params.productId,
+        'GET Product name: ' + req.params.productName+
+        ' GET Product id: '+req.params.productId,
     )
 })
 // GET /products/8719 HTTP/1.1
 app.get('/products/:productId',(req,res)=>{
-    res.send('product page ID: '+req.params.productId)
+    res.send('GET product  ID: '+req.params.productId)
 })
-
-
-
 
 
 app.listen(3000)
