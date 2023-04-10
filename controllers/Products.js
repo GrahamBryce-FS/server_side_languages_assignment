92f39d2dccaa1b8e52aae86fd33153507db0d8db
Bryce Graham <bgraham1@student.fullsail.edu>
2023-04-10 11:37:00 +0900

    cleaned code

---
+++ controllers/Products.js
@@ -0,0 +1,37 @@
+const Products = require('../models/Products')
+
+// curl --location -X GET 'localhost:3000/products' 
+const index = (req,res)=>{
+    const products = Products.all()
+    res.json(products)
+}
+
+const form = (req,res) => {
+    res.send('Product.form')
+}
+
+// curl --location -X GET 'localhost:3000/products/1'
+const show = (req,res) => {
+    const product = Products.find(req.params.id)
+    res.json(product)
+}
+
+// curl -X POST --data "id=2&slug=nike-test&name=Nike Shoe test create" localhost:3000/products
+const create = (req,res) => {
+    const product = Products.create(req.body)
+    res.json(product)
+}
+
+// curl -X POST --data "id=1&slug=nike-shoe&name=Nike Shoe updated" localhost:3000/products/1 
+const update = (req,res) => {
+    const product = Products.update(req.params.id, req.body)
+    res.json(product)
+}
+
+// curl -X DELETE localhost:3000/products/1  
+const remove = (req,res) => {
+    const product = Products.remove(req.params.id)
+    res.json(product)
+}
+
+module.exports = { index, form, show, create, update, remove }
--- index.js
+++ index.js
@@ -1,30 +1,9 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+const productRouter = require('./routes/Products')
 
-app.get('/', (req,res)=>{
-    res.send('Home Page')
-})
-
-// GET /products/all HTTP/1.1
-app.get('/products/all', (req,res)=>{
-    const page = req.query.page || 2;
-    const sort = req.query.sort || 'price';
-    const order = req.query.order || 'desc';
-    const responseString = `GET Products: page=${page}, sort=${sort}, order=${order}`;
-    res.send(responseString);
-})
-
-// GET /products/8719-small-red HTTP/1.1
-app.get('/products/:productId-:productName', (req,res)=>{
-    res.send(
-        'GET Product name: ' + req.params.productName+
-        ' GET Product id: '+req.params.productId,
-    )
-})
-// GET /products/8719 HTTP/1.1
-app.get('/products/:productId',(req,res)=>{
-    res.send('GET product  ID: '+req.params.productId)
-})
-
+app.use('/products',productRouter)
 
 app.listen(3000)
